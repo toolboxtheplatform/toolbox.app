836a8b60dab2231990a7d8b2663ce7d145640a90
Zafar Saleem <zafarsaleem3@gmail.com>
2019-06-22 10:26:15 -0400

    Two new items added to navigation menu

--- src/components/common/navigation/navigationComponent.js
+++ src/components/common/navigation/navigationComponent.js
@@ -3,6 +3,7 @@ import { Link, Route } from 'react-router-dom';
 
 import Home from '../../admin/home/homeComponent';
 import Dashboard from '../../admin/dashboard/dashboardComponent';
+import Application from '../../admin/application/applicationComponent';
 
 import './navigation.scss';
 
@@ -12,8 +13,11 @@ class Navigation extends Component {
       <div className='navigation'>
         <ul>
           <li><Link to={`${this.props.match.path}/dashboard`} className={(this.props.location.pathname.split('/')[2] === 'dashboard') ? 'active' : 'inactive'}>Dashboard</Link></li>
+          <li><Link to={`${this.props.match.path}/add`} className={(this.props.location.pathname.split('/')[2] === 'add') ? 'active' : 'inactive'}>Add Employee</Link></li>
+          <li><Link to={`${this.props.match.path}/app`} className={(this.props.location.pathname.split('/')[2] === 'app') ? 'active' : 'inactive'}>Add Application</Link></li>
         </ul>
         <Route path={`${this.props.match.path}/dashboard`} component={Dashboard} />
+        <Route path={`${this.props.match.path}/app`} component={Application} />
       </div>
     );
   }
