cefe7f2a2bcdd858ee25e56664c52de8efbfa5bd
Zafar Saleem <zafarsaleem3@gmail.com>
2019-06-22 09:39:25 -0400

    Added active state to menu items

--- src/components/common/navigation/navigationComponent.js
+++ src/components/common/navigation/navigationComponent.js
@@ -4,12 +4,14 @@ import { Link, Route } from 'react-router-dom';
 import Home from '../../admin/home/homeComponent';
 import Dashboard from '../../admin/dashboard/dashboardComponent';
 
+import './navigation.scss';
+
 class Navigation extends Component {
   render() {
     return (
-      <div>
+      <div className='navigation'>
         <ul>
-          <li><Link to={`${this.props.match.path}/dashboard`}>Dashboard</Link></li>
+          <li><Link to={`${this.props.match.path}/dashboard`} className={(this.props.location.pathname.split('/')[2] === 'dashboard') ? 'active' : 'inactive'}>Dashboard</Link></li>
         </ul>
         <Route path={`${this.props.match.path}/dashboard`} component={Dashboard} />
       </div>
