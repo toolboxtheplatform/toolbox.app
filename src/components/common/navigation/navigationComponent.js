ebf4523a02fe4c23d0fddbe1f54afccf2297b35d
Zafar Saleem <zafarsaleem3@gmail.com>
2019-06-28 14:50:03 -0400

    Replace dashboard with home component in navigation

--- src/components/common/navigation/navigationComponent.js
+++ src/components/common/navigation/navigationComponent.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link, Route } from 'react-router-dom';
 
 import Home from '../../admin/home/homeComponent';
-import Dashboard from '../../admin/dashboard/dashboardComponent';
+import Dashboard from '../../admin/home/homeComponent';
 import Tool from '../../admin/tool/toolComponent';
 
 import './navigation.scss';
@@ -12,11 +12,11 @@ class Navigation extends Component {
     return (
       <div className='navigation'>
         <ul>
-          <li><Link to={`${this.props.match.path}/dashboard`} className={(this.props.location.pathname.split('/')[2] === 'dashboard') ? 'active' : 'inactive'}>Dashboard</Link></li>
+          <li><Link to={`${this.props.match.path}/home`} className={(this.props.location.pathname.split('/')[2] === 'home') ? 'active' : 'inactive'}>Home</Link></li>
           <li><Link to={`${this.props.match.path}/add`} className={(this.props.location.pathname.split('/')[2] === 'add') ? 'active' : 'inactive'}>Add Employee</Link></li>
           <li><Link to={`${this.props.match.path}/app`} className={(this.props.location.pathname.split('/')[2] === 'app') ? 'active' : 'inactive'}>Add Tool</Link></li>
         </ul>
-        <Route path={`${this.props.match.path}/dashboard`} component={Dashboard} />
+        <Route path={`${this.props.match.path}/home`} component={Dashboard} />
         <Route path={`${this.props.match.path}/app`} component={Tool} />
       </div>
     );
