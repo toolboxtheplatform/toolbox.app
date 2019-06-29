a75c9d154b6731407165c619980bc0dfde28bd80
Zafar Saleem <zafarsaleem3@gmail.com>
2019-06-29 09:37:49 -0400

    Fixed employees text in navigation

--- src/components/common/navigation/navigationComponent.js
+++ src/components/common/navigation/navigationComponent.js
@@ -3,6 +3,7 @@ import { Link, Route } from 'react-router-dom';
 
 import Home from '../../admin/home/homeComponent';
 import Dashboard from '../../admin/home/homeComponent';
+import Employees from '../../admin/employees/employeesComponent';
 import Tool from '../../admin/tool/toolComponent';
 
 import './navigation.scss';
@@ -13,10 +14,11 @@ class Navigation extends Component {
       <div className='navigation'>
         <ul>
           <li><Link to={`${this.props.match.path}/home`} className={(this.props.location.pathname.split('/')[2] === 'home') ? 'active' : 'inactive'}>Home</Link></li>
-          <li><Link to={`${this.props.match.path}/add`} className={(this.props.location.pathname.split('/')[2] === 'add') ? 'active' : 'inactive'}>Add Employee</Link></li>
+          <li><Link to={`${this.props.match.path}/employees`} className={(this.props.location.pathname.split('/')[2] === 'employees') ? 'active' : 'inactive'}>Employees</Link></li>
           <li><Link to={`${this.props.match.path}/app`} className={(this.props.location.pathname.split('/')[2] === 'app') ? 'active' : 'inactive'}>Add Tool</Link></li>
         </ul>
         <Route path={`${this.props.match.path}/home`} component={Dashboard} />
+        <Route path={`${this.props.match.path}/employees`} component={Employees} />
         <Route path={`${this.props.match.path}/app`} component={Tool} />
       </div>
     );
