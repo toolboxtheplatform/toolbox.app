7d141644ecef4545aebf5b29a377b5e484bfe1a2
Zafar Saleem <zafarsaleem3@gmail.com>
2019-06-30 17:57:03 -0400

    imported index.scss file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import configureStore from './store/configureStore';
-import './index.css';
+import './index.scss';
 
 import App from './container/App';
 
