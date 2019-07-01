dc3fb4c49eade31940f8fdfe6654e75ee4c9ef04
Zafar Saleem <zafarsaleem3@gmail.com>
2019-07-01 11:04:02 -0400

    Moved tool component to common folder

---
+++ src/components/common/tool/toolView.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './tool.scss';
+
+const ApplicationView = ({ props, onSubmitHandle, state }) => (
+  <div className='container tool-container'>
+    <ul className={(state.success) ? 'success' : 'errors'}>
+      {
+        (state.message !== undefined)
+        ? state.message.map((error, index) => (
+            <li key={index}>{`${error}`}</li>
+          ))
+        : ''
+      }
+    </ul>
+    <form onSubmit={onSubmitHandle}>
+      <div>
+        <label htmlFor='tool'>Enter Tool Name</label>
+        <input type='text' name='tool' id='tool' autoFocus placeholder='Enter Tool Name such as Jira, Conflunence, Trello, Facebook etc' />
+      </div>
+      <div>
+        <label htmlFor='link'>Enter Home Link</label>
+        <input type='text' name='link' placeholder='Enter Link' id='link' />
+      </div>
+      <div>
+        <button>Save</button>
+      </div>
+    </form>
+  </div>
+);
+
+export default ApplicationView;
