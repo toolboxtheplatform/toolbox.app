c250731ad04b20aca986cfc6556388d41c03720c
Zafar Saleem <zafarsaleem3@gmail.com>
2019-07-11 07:08:08 -0400

    added utility class to logout and fixed tool path in navigation fixed positioning of logout link

--- src/components/common/navigation/navigationComponent.js
+++ src/components/common/navigation/navigationComponent.js
@@ -30,10 +30,10 @@ class Navigation extends Component {
           <ul className='navitation-container'>
             <li><Link to={`${this.props.match.path}/dashboard`} className={(this.props.location.pathname.split('/')[2] === 'dashboard') ? 'active' : 'inactive'}>Dashboard</Link></li>
             <li><Link to={`${this.props.match.path}/profile`} className={(this.props.location.pathname.split('/')[2] === 'profile') ? 'active' : 'inactive'}>Profile</Link></li>
-            <li><Link to={`${this.props.match.path}/app`} className={(this.props.location.pathname.split('/')[2] === 'app') ? 'active' : 'inactive'}>Add Tool</Link></li>
+            <li><Link to={`${this.props.match.path}/tool`} className={(this.props.location.pathname.split('/')[2] === 'app') ? 'active' : 'inactive'}>Add Tool</Link></li>
           </ul>
         }
-        <Link to='/logout' className='logout'>Logout</Link>
+        <Link to='/logout' className='logout danger'>Logout</Link>
         <Route path={`${this.props.match.path}/home`} component={Home} />
         <Route path={`${this.props.match.path}/employees`} component={Employees} />
         <Route path={`${this.props.match.path}/tool`} component={Tool} />
