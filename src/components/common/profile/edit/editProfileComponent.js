9c4aa15cc76e238a97ee0153f3e8fe5f8a7b2e6c
Zafar Saleem <zafarsaleem3@gmail.com>
2019-08-10 02:51:44 -0400

    bug fix for profile

--- src/components/common/profile/edit/editProfileComponent.js
+++ src/components/common/profile/edit/editProfileComponent.js
@@ -29,7 +29,7 @@ class EditProfile extends Component {
 
   componentDidMount() {
     this.setState({
-      employee: this.props.profile.employee,
+      employee: this.props.profile.payload,
     });
   }
 
